fa9c4e20468f62692bb4d454ae1762e1d550f1e7
Aidan <aidankmcalister@gmail.com>
2025-01-18 17:42:20 -0600

    update

--- app/routes/api/cron.js
+++ app/routes/api/cron.js
@@ -1,3 +1,9 @@
-export async function loader() {
+export async function loader(req) {
+  if (
+    req.headers.get("Authorization") !== `Bearer ${import.meta.env.CRON_SECRET}`
+  ) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
   return new Response("Hello Cron!");
 }
